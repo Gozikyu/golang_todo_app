2bd1472edc794bcfed7e3a9fd624a9f871c3fa56
yuta <gozikyu@gmail.com>
2024-02-16 18:48:15 +0900

    tailwindをインストール

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -1,10 +1,22 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    node: true,
+    es6: true,
+  },
+  parserOptions: { ecmaVersion: 8, sourceType: 'module' },
   extends: [
     'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:prettier/recommended',
+    'plugin:testing-library/react',
+    'plugin:jest-dom/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
@@ -15,4 +27,4 @@ module.exports = {
       { allowConstantExport: true },
     ],
   },
-}
+};
